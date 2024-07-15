c6fcd77d14cf712fd139ddbccc10247bdcffe39d
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2024-07-15 22:50:31 +0700

    Removed dist folder

--- dist/utils/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockUsers = void 0;
-exports.mockUsers = [
-    { id: 1, username: "anson", displayName: "Anson", password: "hello123" },
-    { id: 2, username: "jack", displayName: "Jack", password: "hello124" },
-    { id: 3, username: "adam", displayName: "Adam", password: "hellohello" },
-];
--- dist/utils/global_middlewares_initialzer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middlewares_1 = require("./middlewares");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.use(middlewares_1.loggingMiddlware);
-exports.default = router;
